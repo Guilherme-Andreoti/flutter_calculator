8810e610a7e1d1a183f1665ec4be7d62036bcf98
Guilherme De Souza Andreoti <0040481911010@americana.fatec.br>
2020-03-03 10:29:30 -0300

    Starting the project

---
+++ lib/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calcuator extends StatefulWidget {
+  @override
+  _CalcuatorState createState() => _CalcuatorState();
+}
+
+class _CalcuatorState extends State<Calcuator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/memory.dart
@@ -0,0 +1,5 @@
+class Memory {
+  String result = '0';
+   
+
+}
