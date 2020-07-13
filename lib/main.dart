ff014d135b61d6633a121677d01ac19a61329414
Guilherme Andreoti <guilherme.andreoti@outlook.com>
2020-07-13 11:54:31 -0300

    Add Splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,46 @@
 import 'package:calculator/calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      
+      seconds: 5,
+      navigateAfterSeconds: new AfterSplash(),
+      title: new Text(
+        'Bem vindo a calculadora 1.0',
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 20.0
+        ),
+      ),
+      image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      loaderColor: Colors.red
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,3 +49,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home:Calcuator()
+//     );
+//   }
+// }
